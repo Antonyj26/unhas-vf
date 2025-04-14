bdb2346893e256553871989f33a121159f57733e
Antonyj26 <antonyjosu26@gmail.com>
2025-04-14 15:42:51 -0300

    Fazendo a estilização da tela de edit

--- src/components/Edit/Edit.tsx
+++ src/components/Edit/Edit.tsx
@@ -1,3 +1,30 @@
+import './Edit.css';
+
 export function Edit() {
-  return <div>Componente editar</div>;
+  return (
+    <div className="edit-container">
+      <h1>Criar agendamento</h1>
+      <form className="form">
+        <label htmlFor="name">Nome do cliente:</label>
+        <input type="text" id="name" placeholder="Ex: Vitória França" />
+        <label htmlFor="date">Selecione a Data:</label>
+        <input type="date" id="date" />
+        <label htmlFor="number">Contato:</label>
+        <input type="number" id="number" placeholder="Ex: (00) 000000000" />
+        <label htmlFor="service">Serviço</label>
+        <select name="service" id="service">
+          <option value="" disabled selected>
+            Selecione o Serviço
+          </option>
+
+          <option value="Completo">Completo</option>
+          <option value="Manicure">Manicure</option>
+          <option value="Pedicure">Pedicure</option>
+        </select>
+        <button>Salvar</button>
+        <button>Deletar</button>
+        <button>Cancelar</button>
+      </form>
+    </div>
+  );
 }
--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,3 +1,5 @@
+import './List.css';
+
 export function List() {
   return (
     <div>
